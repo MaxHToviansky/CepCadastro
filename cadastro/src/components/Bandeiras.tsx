4fa4aa8fb49c539fe97696cfd9d1ac70072300fa
ZeppZeppelin <mtoviansky@gmail.com>
2022-09-20 18:16:58 -0300

    Fixed bug
    Quando um cep invalido é inserido a imagem quebra.

--- cadastro/src/components/Bandeiras.tsx
+++ cadastro/src/components/Bandeiras.tsx
@@ -4,8 +4,9 @@ import BR from "../assets/Bandeiras/BR.png"
 
 export default function() {
     const { uf } = React.useContext(EnderecoContext)
-    if (uf == ""){
-        return <div id="imgBandeira"><img src={BR}/></div>
-    }
+    if (uf == "") return <div id="imgBandeira"><img src={BR}/></div>
+    
+    if(uf == undefined) return <div id="imgBandeira"><img src={BR}/></div>
+
     return <div id="imgBandeira"><img src={`https://raw.githubusercontent.com/bgeneto/bandeiras-br/master/imagens/${uf}.png`}/></div>
 }
--- cadastro/src/components/InputCep.tsx
+++ cadastro/src/components/InputCep.tsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 import { EnderecoContext } from "../pages/EnderecoContext";
 
 export default function () {
-    const { cep, setCep, setRua, setCidade, setUf } = React.useContext(EnderecoContext)
+    const { cep, setCep, setRua, setCidade, uf, setUf } = React.useContext(EnderecoContext)
+    const [error, setError] = React.useState(false)
 
     const updateCep = async (ev: React.KeyboardEvent<HTMLInputElement>) => {
         if (ev.currentTarget.value.length < 9) return
@@ -15,6 +16,13 @@ export default function () {
     const BuscarCep = async (cep: string) => {
         const getCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         const responseCep = await getCep.json()
+        if (responseCep.erro) {
+            setError(responseCep.erro)
+            console.log(`Cep Invalido. Error = ${error}`)
+            return
+            // Por algum motivo o codigo diz que o erro não ocorreu na primeira tentativa
+        }
+        setError(false)
         setUf(responseCep.uf)
         setCidade(responseCep.localidade)
         setRua(responseCep.logradouro)
